8bad5d20459b65fe2e16a1ccab4ce0bf8debfdd9
vyadzmak <vyadzmak@gmail.com>
2017-11-29 14:54:10 +0300

    minor fixes schema creation

--- models/app_models/variable_models/int_variable_model.py
+++ models/app_models/variable_models/int_variable_model.py
@@ -2,5 +2,5 @@ class IntVar():
     def __init__(self,not_null = False,default_value = 0,max_value =-1,min_value =-1):
         self.not_null = False
         self.default_value = 0
-        self.max_value =-1
-        self.min_value =-1
+        self.max_value =max_value
+        self.min_value =min_value

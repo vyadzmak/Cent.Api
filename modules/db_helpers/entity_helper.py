4b9df071b8a8ffe31c78659c77d7c22424f31562
vyadzmak <vyadzmak@gmail.com>
2017-12-01 20:30:07 +0300

    fixed items

--- modules/db_helpers/entity_helper.py
+++ modules/db_helpers/entity_helper.py
@@ -1,4 +1,5 @@
 from db.db import session
+import json
 import models.app_models.dynamic_object_models.dynamic_object_model as d_object
 def get_entity_fields_by_id(id):
     from models.db_models.models import Objects
@@ -12,26 +13,31 @@ def get_entity_fields_by_id(id):
 
 
 def get_items(object_id, type_id):
+
+
     from models.db_models.models import Schemas
     from models.db_models.models import Objects
     items = []
-    #s = session.query(Schemas).all()
-    #o = session.query(Objects).all()
-    elements = session.query(Objects, Schemas)\
-        .filter(Objects.schema_id==Schemas.id)\
-        .filter(Schemas.schema_type_id==type_id)\
-        .filter(Objects.parent_id==object_id)\
-        .all()
-    indexes =[]
-    for element in elements:
-        ob = d_object.DynamicObject()
-        sc = element[1]
-        t = sc.id in indexes
-        if (t ==False):
-            indexes.append(sc.id)
-            setattr(ob,'id',sc.id)
-            setattr(ob,'name',sc.title)
-            items.append(ob)
+    object = session.query(Objects).filter(Objects.id ==object_id).first()
+
+    schema= session.query(Schemas).filter(Schemas.id==object.schema_id).first()
+
+    data = json.loads(schema.data)
+    fields = data["fields"]
+
+    for f in fields:
+        if (f["field_type"]==5):
+            v = f["var"]
+            id =v["schema_id"]
+            nm =v["title"]
+
+            t_schema = session.query(Schemas).filter(Schemas.id==id).first()
+            if (t_schema.schema_type_id==type_id):
+                ob = d_object.DynamicObject()
+                setattr(ob, 'id', id)
+                setattr(ob, 'name', nm)
+                items.append(ob)
+
 
     return items
 
